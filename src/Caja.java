a34bcf9363730088ff242eab65e64ba45d28ac40
apineiroandion <apineiroandion@danielcastelao.org>
2024-10-10 09:53:49 +0200

    Caja

---
+++ src/Caja.java
@@ -0,0 +1,15 @@
+public class Caja {
+    private Double dinero;
+
+    public Caja(Double dinero) {
+        this.dinero = dinero;
+    }
+
+    public Double getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(Double dinero) {
+        this.dinero = dinero;
+    }
+}
